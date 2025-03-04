0df73dd49ae6bbb0f58b34660f67956d2ba93bc1
Nik Lussy <nik-lussy@hotmail.com>
2025-03-04 16:21:39 +0100

    updated guess Number

--- bashTry/guessNumberSimulationV2.sh
+++ bashTry/guessNumberSimulationV2.sh
@@ -25,8 +25,7 @@ function runSim(){
   writeHeader
   for ((sim = 0 ; sim < $simEnd ; sim++)); do
     for ((currentTry = 0 ; currentTry < $maxTry ; currentTry++)); do
-      let guesses=$(( RANDOM % (max - min + 1) + min ))
-
+      guesses=$(( RANDOM % (max - min + 1) + min ))  # Generate a new guess
       if [ $guesses -eq $random_number ]; then
         break
       fi
@@ -39,19 +38,12 @@ function runSim(){
 function readResults(){
   arr_sim=( $(tail -n +2 $fileName | cut -d ',' -f1) )
   arr_guesses=( $(tail -n +2 $fileName | cut -d ',' -f2) )
-  # while IFS="," read -r line
-  # do
-  #   total2=$((total2+line))
-  # done < <(cut -d "," -f2 $fileName |tail -n +2)
-  # echo Average: $((total/1000))
-  # echo Average2: $((total2/1000))
 }
 
 function calcDiff(){
   Average=$((total/simEnd))
   for ((sim = 0 ; sim < $simEnd ; sim++)); do
     arr_diff[$sim]=$((arr_guesses[sim]-$Average))
-    # echo "${arr_diff[sim]}"
   done
 }
 
@@ -63,10 +55,6 @@ function WriteFileWithDiff(){
    writeLine "" $Average ""
 }
 function printTopTen(){
-  # cat $fileName | tail -n +2 | sort -t, -k2,2n | head -10
-  # awk -F, 'NR>1 {print $0, ($3<0)?-$3:$3}' $fileName | sort -k4,4n -t' ' | head -10 | cut -d ' ' -f1-3
-  # awk -F, 'NR>1 && $1+0 <= 1000 {print $0, ($3<0)?-$3:$3}' $fileName | sort -k4,4n -t' ' | cut -d ' ' -f1-3 | head -10
-
   echo "Alle Resultate nahe bei der Abweichung";
   awk -F, '{print $1, $2, $3, sqrt($3^2)}' $fileName | sort -k4,4n -t' ' | head -12 | tail -10 | sort -k1,4n -t' ' | cut -d ' ' -f1-3;
 
